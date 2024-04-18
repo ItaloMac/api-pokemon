b39c27854e715cb10a5cbe652bf231da687ad956
italo macedo <macedoitalo21@gmail.com>
2024-04-18 17:40:22 -0500

    Correção function detalhesPokemon

--- src/controladores/pokemons.js
+++ src/controladores/pokemons.js
@@ -7,18 +7,29 @@ const listagemPokemons = async (req,res) => {
 }
 
 const detalhesPokemon = async (req,res) => {
-    const { id } = req.params;
+    const id = req.params.id
     
     try {
-        const pokemonBuscado = await detalharPokemon(id)
-    
+        const pokemonBuscado =  await detalharPokemon(Number(id))
+        
         if (!pokemonBuscado) {
             return res.status(404).json({ error: 'Pokemon não encontrado' });
         }
 
-        const { id, name, height, weight, base_experience, forms, abilities, species } = pokemonBuscado;
+        const {id: pokemonID, name, height, weight, base_experience, forms, abilities, species } = pokemonBuscado;
+
+        const informacoesPokemon  = {
+            pokemonID,
+            name,
+            height,
+            weight,
+            base_experience,
+            forms,
+            abilities,
+            species
+        }
         
-        res.json(produtoBuscado);
+        res.json(informacoesPokemon);
     } catch (error) {
         console.error('Erro ao detalhar o Pokémon:', error);
         res.status(500).json({ error: 'Erro ao detalhar o Pokémon' }); 
